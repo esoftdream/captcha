7a1a3020c8aa48fa36ac7119a0374197709d3517
Toto <totoprayogo1916@gmail.com>
2025-08-08 11:12:50 +0700

    ✨ feat(image): tingkatkan keamanan captcha dengan distorsi dan noise
    - 【Fitur】 Mengubah jenis string acak menjadi numerik untuk keamanan.
    - 【Fitur】 Menambahkan noise titik dan garis untuk mempersulit bot.
    - 【Fitur】 Menerapkan transformasi gelombang untuk distorsi gambar.
    - 【Fitur】 Menambahkan fungsi helper untuk menghasilkan frekuensi, fase, dan ukuran acak.

--- src/Image.php
+++ src/Image.php
@@ -104,7 +104,7 @@ class Image
         $this->id = $id;
 
         // Campur huruf & angka
-        $word = random_string('alnum', $this->wordLength);
+        $word = random_string('numeric', $this->wordLength);
         $this->word = strtolower($word);
 
         return $id;
@@ -121,36 +121,137 @@ class Image
             throw new Exception('Valid font file is required for CAPTCHA');
         }
 
-        $w = $this->width;
-        $h = $this->height;
-        $fontSize = mt_rand($this->fontSize - 2, $this->fontSize + 2);
+        $w     = $this->width;
+        $h     = $this->height;
+        $fsize = mt_rand($this->fontSize - 2, $this->fontSize + 2);
 
         $imgFile = $this->imgDir . $id . $this->suffix;
         $img = imagecreatetruecolor($w, $h);
 
-        $bgColor = imagecolorallocate($img, mt_rand(230, 255), mt_rand(230, 255), mt_rand(230, 255));
-        $textColor = imagecolorallocate($img, mt_rand(0, 50), mt_rand(0, 50), mt_rand(0, 50));
 
-        imagefilledrectangle($img, 0, 0, $w, $h, $bgColor);
+        $textColor = imagecolorallocate($img, 0, 0, 0);
+        $bgColor   = imagecolorallocate($img, 255, 255, 255);
+        imagefilledrectangle($img, 0, 0, $w - 1, $h - 1, $bgColor);
+        $textbox = imageftbbox($fsize, 0, $this->font, $word);
+        $x       = ($w - ($textbox[2] - $textbox[0])) / 2;
+        $y       = ($h - ($textbox[7] - $textbox[1])) / 2;
+        $x       = (int) $x;
+        $y       = (int) $y;
+        imagefttext($img, $fsize, 0, $x, $y, $textColor, $this->font, $word);
 
-        $textbox = imageftbbox($fontSize, 0, $this->font, $word);
-        $x = (int)(($w - ($textbox[2] - $textbox[0])) / 2);
-        $y = (int)(($h - ($textbox[7] - $textbox[1])) / 2);
-
-        imagefttext($img, $fontSize, mt_rand(-10, 10), $x, $y, $textColor, $this->font, $word);
-
-        // Noise titik
+        // generate noise
         for ($i = 0; $i < $this->dotNoiseLevel; $i++) {
             imagefilledellipse($img, mt_rand(0, $w), mt_rand(0, $h), 2, 2, $textColor);
         }
-
-        // Noise garis
         for ($i = 0; $i < $this->lineNoiseLevel; $i++) {
             imageline($img, mt_rand(0, $w), mt_rand(0, $h), mt_rand(0, $w), mt_rand(0, $h), $textColor);
         }
 
-        imagepng($img, $imgFile);
+        // transformed image
+        $img2    = imagecreatetruecolor($w, $h);
+        $bgColor = imagecolorallocate($img2, 255, 255, 255);
+        imagefilledrectangle($img2, 0, 0, $w - 1, $h - 1, $bgColor);
+
+        // apply wave transforms
+        $freq1 = $this->randomFreq();
+        $freq2 = $this->randomFreq();
+        $freq3 = $this->randomFreq();
+        $freq4 = $this->randomFreq();
+
+        $ph1 = $this->randomPhase();
+        $ph2 = $this->randomPhase();
+        $ph3 = $this->randomPhase();
+        $ph4 = $this->randomPhase();
+
+        $szx = $this->randomSize();
+        $szy = $this->randomSize();
+
+        for ($x = 0; $x < $w; $x++) {
+            for ($y = 0; $y < $h; $y++) {
+                $sx = $x + (sin($x * $freq1 + $ph1) + sin($y * $freq3 + $ph3)) * $szx;
+                $sy = $y + (sin($x * $freq2 + $ph2) + sin($y * $freq4 + $ph4)) * $szy;
+                $sx = (int) $sx;
+                $sy = (int) $sy;
+
+                if ($sx < 0 || $sy < 0 || $sx >= $w - 1 || $sy >= $h - 1) {
+                    continue;
+                } else {
+                    $color   = (imagecolorat($img, $sx, $sy) >> 16) & 0xFF;
+                    $colorX  = (imagecolorat($img, $sx + 1, $sy) >> 16) & 0xFF;
+                    $colorY  = (imagecolorat($img, $sx, $sy + 1) >> 16) & 0xFF;
+                    $colorXY = (imagecolorat($img, $sx + 1, $sy + 1) >> 16) & 0xFF;
+                }
+
+                if ($color === 255 && $colorX === 255 && $colorY === 255 && $colorXY === 255) {
+                    // ignore background
+                    continue;
+                } elseif ($color === 0 && $colorX === 0 && $colorY === 0 && $colorXY === 0) {
+                    // transfer inside of the image as-is
+                    $newcolor = 0;
+                } else {
+                    // do antialiasing for border items
+                    $fracX  = $sx - floor($sx);
+                    $fracY  = $sy - floor($sy);
+                    $fracX1 = 1 - $fracX;
+                    $fracY1 = 1 - $fracY;
+
+                    $newcolor = $color * $fracX1 * $fracY1
+                              + $colorX * $fracX * $fracY1
+                              + $colorY * $fracX1 * $fracY
+                              + $colorXY * $fracX * $fracY;
+                }
+
+                imagesetpixel($img2, $x, $y, imagecolorallocate(
+                    $img2,
+                    (int) $newcolor,
+                    (int) $newcolor,
+                    (int) $newcolor
+                ));
+            }
+        }
+
+        // generate noise
+        for ($i = 0; $i < $this->dotNoiseLevel; $i++) {
+            imagefilledellipse($img2, mt_rand(0, $w), mt_rand(0, $h), 2, 2, $textColor);
+        }
+
+        for ($i = 0; $i < $this->lineNoiseLevel; $i++) {
+            imageline($img2, mt_rand(0, $w), mt_rand(0, $h), mt_rand(0, $w), mt_rand(0, $h), $textColor);
+        }
+
+        imagepng($img2, $imgFile);
         imagedestroy($img);
+        imagedestroy($img2);
+    }
+
+    /**
+     * Generate random frequency
+     *
+     * @return float
+     */
+    protected function randomFreq()
+    {
+        return mt_rand(700000, 1000000) / 15000000;
+    }
+
+    /**
+     * Generate random phase for distortion
+     *
+     * @return float Random phase value.
+     */
+    protected function randomPhase()
+    {
+        return mt_rand(0, 3141592) / 1000000;
+    }
+
+    /**
+     * Generate random character size for distortion
+     *
+     * @return float|int Random size value.
+     */
+    protected function randomSize()
+    {
+        return mt_rand(300, 700) / 100;
     }
 
     protected function cleanupOldCaptchas(): void
